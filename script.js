420b80201c226b0ffd6bf88c86de734e3d4175fc
khosro akbari <ite.akbari@gmail.com>
2025-01-28 16:53:12 +0330

    add form

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const reedCheck = document.querySelector(".label-check input");
 const reedDate = document.getElementById("reedDate");
+const lbls = [...document.querySelectorAll(".input-label")];
+const form = document.getElementById("submitForm");
+
 reedCheck.addEventListener("change", function () {
   this.checked
     ? (reedDate.style.display = "grid")
